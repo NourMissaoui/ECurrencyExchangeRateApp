74e7061a7cd15f1cca1dc01a5383b8d6d9af8a1f
nour_missaoui <mno@mobilepowered.fr>
2022-04-18 10:31:45 +0200

    fix code

--- app/src/main/java/com/example/ecurrencyexchangeapp/utils/AppConstant.kt
+++ app/src/main/java/com/example/ecurrencyexchangeapp/utils/AppConstant.kt
@@ -2,5 +2,5 @@ package com.example.ecurrencyexchangeapp.utils
 
 object AppConstant {
     const val BASE_URL = "http://api.exchangeratesapi.io/v1/"
-    const val API_access_Key = "2067add34da9faf7eca3818ee854fbaa"
+    const val API_access_Key = "84cf4b253d071215a6f79fde9d087031"
 }
